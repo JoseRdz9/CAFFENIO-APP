a919d7f48b4c65494b68180aa01cf31bb4126a13
Sergio Soto <oigres3.ss@gmail.com>
2025-04-03 09:41:15 -0600

    notify

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -1,7 +1,9 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
-
 import { LoginComponent } from './login.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { FormsModule } from '@angular/forms';
+import { AuthService } from '../common/services/auth.service';  // Asegúrate de tener el servicio en tu test si es necesario
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -10,7 +12,12 @@ describe('LoginComponent', () => {
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ LoginComponent ],
-      imports: [IonicModule.forRoot()]
+      imports: [
+        IonicModule,
+        RouterTestingModule,  // Importa RouterTestingModule para probar la navegación
+        FormsModule  // Importa FormsModule para que ngModel funcione
+      ],
+      providers: [AuthService]  // Asegúrate de tener el servicio disponible si es necesario
     }).compileComponents();
 
     fixture = TestBed.createComponent(LoginComponent);
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,6 +3,8 @@ import { FormsModule } from '@angular/forms'; // Importa FormsModule
 import { Component } from '@angular/core';
 import { AuthService } from '../common/services/auth.service';
 import { Router } from '@angular/router';
+import Toastify from 'toastify-js';
+import 'toastify-js/src/toastify.css';
 
 
 @Component({
@@ -18,16 +20,26 @@ export class LoginComponent {
   constructor(private authService: AuthService, private router: Router) {}
 
   login() {
-    console.log('Login attempt for:', this.email);  // Agrega un log para ver si se está llamando al método
-
+    console.log('Login attempt for:', this.email);  // Log para verificar si el método se ejecuta
+  
     this.authService.login(this.email, this.password).subscribe(
       (result) => {
         console.log('Login successful', result);
-        this.router.navigate(['/tabs/home']); // Redirige a la página principal después del login exitoso
+        this.router.navigate(['/tabs/home']); // Redirige al home si es exitoso
       },
       (error) => {
         console.error('Login failed', error);
+        
+        // Crear una nueva instancia de Toastify sin la propiedad `style`
+        new Toastify({
+          text: "Correo o contraseña incorrectos",
+          duration: 3000,
+          gravity: "top", // "top" o "bottom"
+          position: "right", // "left", "center", "right"
+          backgroundColor: "#dc1436"
+        }).showToast();
       }
     );
   }
-}
+  
+  }
